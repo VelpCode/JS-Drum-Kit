6fffa76e071d02482edeaa1b44ccb3eef6f3e41f
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2022-09-13 19:45:44 -0400

    Customizing page layout

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-}
-
-function playSound(e) {
+window.addEventListener('keydown', function (e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;
-
-
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
-}
-
-const keys = Array.form(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
+    console.log(audio);
+})
